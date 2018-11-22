d4081ee15b917219bd60ea1c3fcf5a51088c1149
xiejava <shygiant@126.com>
2018-11-22 23:07:51 +0800

    定义用户角色

--- app/models.py
+++ app/models.py
@@ -1,8 +1,8 @@
 #-*-coding:utf-8-*-
-from . import db
+from . import db,login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask_login import UserMixin
+from flask_login import UserMixin,AnonymousUserMixin
 from flask import current_app
 
 class Permission:
@@ -16,9 +16,52 @@ class Role(db.Model):
     __tablename__='roles'
     id=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(64),unique=True)
+    default=db.Column(db.Boolean,default=False)
+    permissions=db.Column(db.Integer)
+    users=db.relation('User',backref='role',lazy='dynamic')
+
+    def __int__(self,**kwargs):
+        super(Role, self).__int__(**kwargs)
+        if self.permissions is None:
+            self.permissions=0
+
+    @staticmethod
+    def insert_roles():
+        roles={
+            'User':(Permission.FOLLOW,Permission.COMMENT,Permission.WRITE),
+            'Moderator':(Permission.FOLLOW,Permission.COMMENT,Permission.WRITE,Permission.MODERATE),
+            'Administrator':(Permission.FOLLOW,Permission.COMMENT,Permission.WRITE,Permission.MODERATE,Permission.ADMIN)
+        }
+        default_role='User'
+        for r in roles:
+            role=Role.query.filter_by(name=r).first()
+            if role is None:
+                role=Role(name=r)
+            role.reset_permissions()
+            for perm in roles[r]:
+                role.add_permission(perm)
+            role.default=(role.name==default_role)
+            db.session.add(role)
+        db.session.commit()
+
+    def add_permission(self,perm):
+        if not self.has_permisson(perm):
+            self.permissions+=perm
+
+    def remove_permission(self,perm):
+        if self.has_permisson(perm):
+            self.permissions-=perm
+
+    def reset_permissions(self):
+        self.permissions=0
+
+    def has_permisson(self,perm):
+        return self.permissions & perm==perm
 
     def __repr__(self):
         return '<Role %r>' % self.name
+
+
 
 class User(UserMixin,db.Model):
     __tablename__='users'
@@ -28,6 +71,14 @@ class User(UserMixin,db.Model):
     password_hash=db.Column(db.String(128))
     role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
     confirmed=db.Column(db.Boolean,default=False)
+
+    def __int__(self,**kwargs):
+        super(User,self).__init__(**kwargs)
+        if self.role is None:
+            if self.email==current_app.config['FLASKY_ADMIN']:
+                self.role=Role.query.filter_by(permissions='Administrator').first()
+            if self.role is None:
+                self.role=Role.query.filter_by(default=True).first()
 
     @property
     def password(self):
@@ -63,6 +114,21 @@ class User(UserMixin,db.Model):
         db.session.commit()
         return True
 
+    def can(self,perm):
+        return self.role is not None and self.role.has_permisson(perm)
+
+    def is_administrator(self):
+        return self.can(Permission.ADMIN)
+
     def __repr__(self):
         return '<User %r>' % self.username
 
+
+class AnonymousUser(AnonymousUserMixin):
+    def can(self,permissions):
+        return False
+
+    def is_administrator(self):
+        return False
+
+login_manager.anonymous_user=AnonymousUser
